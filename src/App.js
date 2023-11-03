a3e240664da3538c3a8b9048be16fc53f289bbf2
dh-teja2047 <[dh.teja2047@gmail.com]>
2023-11-03 12:21:43 +0530

    added registration page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,54 @@
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+
+const initialValues = {name :"", userName :"", email :"",mobile :""};
+const [formValues, setFormValues] = useState(initialValues);
+const [formErrors, setFormErrors] = useState({});
+const [isSubmit, setIsSubmit] = useState(false);
+
+
+const handleChange = (e) =>{
+  const {name, value} = e.target;
+  setFormValues({ ...formValues, [name]: value });
+  console.log(formValues);
+};
+
+const handleSubmit = (e) =>{
+  e.preventDefault();
+  setFormErrors(validate(formValues));
+  setIsSubmit(true);
+};
+
+
+useEffect(() => {
+  console.log(formErrors);
+  if (Object.keys(formErrors).length === 0 && isSubmit) {
+    console.log(formValues);
+  }
+}, [formErrors]);
+
+const validate = (values) =>{
+  const errors ={};
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+  if (!values.name) {
+    errors.name = "Field is required!";
+  }
+  if (!values.userName) {
+    errors.userName = "Field is required!";
+  }
+  if (!values.email) {
+    errors.email = "Field is required!";
+  }
+  if (!values.mobile) {
+    errors.mobile = "Field is required!";
+  }
+
+  return errors;
+
+};
+
   return (
     <div className="registration">
       <div className='homePage'>
@@ -13,16 +61,57 @@ function App() {
       <div className='rightPanel'>
         <div className='form'>
         <h2>Super app</h2>
-        <p>Create your new account </p>
-        <form className='formElements'>
-          <input id='name' className='name' placeholder='Name'  /> <br />
-          <input id='userName' className='userName' placeholder='UserName'/> <br />
-          <input id='email' className='email' placeholder='email' /> <br />
-          <input id='mobile' className='mobile' placeholder='Mobile' /> <br />
+        <p className="createAccountPara">Create your new account </p>
+        <form className='formElements' onSubmit={handleSubmit}>
+          <div id='name' className='name' >
+          <input 
+            type='text'
+            name= 'name'
+            placeholder='Name'  
+            value={formValues.name}
+            onChange={handleChange}
+          /> 
+          </div>
+          <p className='errors'>{formErrors.name}</p>
+
+          <div  id='userName' className='userName' >
+          <input 
+           
+            type='text'
+            name='userName'
+            placeholder='UserName'
+            value={formValues.userName}
+            onChange={handleChange}
+          />
+          </div> 
+          <p className='errors'>{formErrors.userName}</p>
+
+          <div id='email' className='email'> 
+          <input 
+            type='text'
+            name='email'
+            placeholder='email' 
+            value={formValues.email}
+            onChange={handleChange}
+          /> 
+          </div>
+          <p className='errors'>{formErrors.email}</p>
+          
+          <div  id='mobile' className='mobile' >
+          <input 
+            type='text'
+            name='mobile'
+            placeholder='Mobile' 
+            value={formValues.mobile}
+            onChange={handleChange}
+            /> 
+          </div>
+          <p className='errors'>{formErrors.mobile}</p>
+          
           <input type="checkbox" id="terms" />
           <label htmlFor="terms" className='terms'>Share my registration data with Superapp</label>
+          <button id='signup' className='signup'>SIGN UP</button> <br /><br />
         </form>
-        <button id='signup' className='signup'>SIGN UP</button> <br /><br />
         <div className='termsAndPolicy'>
         <p> By clicking on Sign up. you agree to Superapp <span style={{color: 'rgba(114, 219, 115, 1)'}}> Terms and Conditions of Use</span></p> <br/><br/><br/>
         <p>To learn more about how Superapp collects, uses, shares and protects your personal data please head Superapp <span style={{color: 'rgba(114, 219, 115, 1)'}}>Privacy Policy</span></p>
